5d52792ff3d5baf42bbe7572cec76cae5595581f
bingli <bing.li@ess.eu>
2026-05-18 15:21:22 +0200

    implemented histogramming

--- reduction/trex_reduction.py
+++ reduction/trex_reduction.py
@@ -6,6 +6,57 @@ from scipp.typing import VariableLike
 from scippneutron.conversion.beamline import _canonical_length
 from scipy.ndimage import label
 
+
+# generate bin boxes
+
+
+def _make_bins(spec, dim, unit="1/Å"):
+    if not isinstance(spec, tuple):
+        raise TypeError(f"{dim} must be a tuple of length 2 or 3")
+
+    if len(spec) == 2:
+        start, stop = spec
+        step = stop - start
+
+    elif len(spec) == 3:
+        start, stop, step = spec
+
+    else:
+        raise ValueError(f"{dim} tuple must have length 2 or 3")
+
+    return sc.arange(dim=dim, start=start, stop=stop + step, step=step, unit=unit)
+
+
+def generate_bins(qx=None, qy=None, qz=None, en=None):
+    """
+    Generate Scipp bin edges for x, y, z, and en.
+
+    Each argument must be:
+      - (min, max)
+      - (start, stop, step)
+
+    Returns
+    -------
+    dict[str, sc.Variable]
+        Bin edges per dimension
+    """
+    bins = {}
+
+    if qx is not None:
+        bins["qx"] = _make_bins(qx, "qx")
+
+    if qy is not None:
+        bins["qy"] = _make_bins(qy, "qy")
+
+    if qz is not None:
+        bins["qz"] = _make_bins(qz, "qz")
+
+    if en is not None:
+        bins["en"] = _make_bins(en, "en", unit="meV")
+
+    return bins
+
+
 # Utility Functions
 
 
@@ -67,7 +118,6 @@ def produce_trex_event_object(
 
     tof_to_centroid = sc.lookup(look_up_tab, mode="previous")
     event_object = event_object.transform_coords(time_on_monitor=tof_to_centroid)
-    event_object.coords["monitor_counts"] = sc.scalar(value=monitor.Ncount.sum())
 
     return event_object
 

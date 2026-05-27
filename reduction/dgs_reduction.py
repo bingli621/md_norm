6d5235a3695e2dbe688acde28b9134bb5efb79a9
bingli <bing.li@ess.eu>
2026-05-27 17:21:57 +0200

    WIP calculate kf extrema

--- reduction/dgs_reduction.py
+++ reduction/dgs_reduction.py
@@ -1,63 +1,13 @@
 import numpy as np
-import mcstasscript as ms
-import mcstastox as mx
 import scipp as sc
+import scipp.constants as consts
 from scipy.ndimage import label
-
-
-# generate bin boxes
-
-
-def _make_bins(spec, dim, unit="1/Å"):
-    if not isinstance(spec, tuple):
-        raise TypeError(f"{dim} must be a tuple of length 2 or 3")
-
-    if len(spec) == 2:
-        start, stop = spec
-        step = stop - start
-
-    elif len(spec) == 3:
-        start, stop, step = spec
-
-    else:
-        raise ValueError(f"{dim} tuple must have length 2 or 3")
-
-    return sc.arange(dim=dim, start=start, stop=stop + step, step=step, unit=unit)
-
-
-def generate_bins(qx=None, qy=None, qz=None, en=None):
-    """
-    Generate Scipp bin edges for x, y, z, and en.
-
-    Each argument must be:
-      - (min, max)
-      - (start, stop, step)
-
-    Returns
-    -------
-    dict[str, sc.Variable]
-        Bin edges per dimension
-    """
-    bins = {}
-
-    if qx is not None:
-        bins["qx"] = _make_bins(qx, "qx")
-
-    if qy is not None:
-        bins["qy"] = _make_bins(qy, "qy")
-
-    if qz is not None:
-        bins["qz"] = _make_bins(qz, "qz")
-
-    if en is not None:
-        bins["en"] = _make_bins(en, "en", unit="meV")
-
-    return bins
 
 
 # Utility Functions
 
 
+# FIXME
 def _calc_pulse_centroid(tof_monitor, threshold=0, to_s=1e-6):
     # Assumes all pulses are evenly spaced in xaxis - but isn't bin data??? confirm this
     # Confirm correct way to axis tof data - is xaxis correct?????
@@ -69,20 +19,19 @@ def _calc_pulse_centroid(tof_monitor, threshold=0, to_s=1e-6):
     weight_total = np.bincount(labels, weights=tof_monitor.Intensity)[1:]
 
     coms = weighted_sum / weight_total * to_s
-    coms = sc.array(dims=["tof"], values=coms, unit="s")
-    return coms
+    # coms = sc.array(dims=["tof"], values=coms, unit="s")
+    return coms[0]
 
 
-# ----------------------- Sciline Functions --------------------------------
+# -----------------------------------------------------
+# Calculate Ei
+# -----------------------------------------------------
 
 
 def source_to_monitor(source_position, monitor_position):
     """ """
     d_sm = (monitor_position).to(unit="m") - (source_position).to(unit="m")
     return sc.norm(d_sm)
-
-
-# -------- ki ---------
 
 
 def unit_vec_ki(monitor_position, sample_position):
@@ -92,16 +41,35 @@ def unit_vec_ki(monitor_position, sample_position):
     return unit_vec_ki
 
 
-def vi_from_one_monitor(monitor_position, time_on_monitor):
+def vi_from_one_monitor(source_to_monitor, time_on_monitor):
     """ """
-    vi = sc.norm(monitor_position) / time_on_monitor
+    vi = source_to_monitor / time_on_monitor
     return vi
+
+
+def ei(vi):
+    ei = 0.5 * consts.m_n * vi**2
+    return sc.to_unit(ei, "meV")
 
 
 def mag_ki(vi):
     """ """
-    mag_ki = (sc.constants.neutron_mass * vi) / sc.constants.hbar
+    mag_ki = (consts.m_n * vi) / consts.hbar
     return sc.to_unit(mag_ki, "1/Å")
+
+
+def monitor_to_sample(
+    sample_position,
+    monitor_position,
+):
+    d = sample_position.to(unit="m") - monitor_position.to(unit="m")
+    return sc.norm(d)
+
+
+def time_on_sample(monitor_to_sample, time_on_monitor, vi):
+    """ """
+    time_on_sample = time_on_monitor + monitor_to_sample / vi
+    return time_on_sample
 
 
 def vec_ki(mag_ki, unit_vec_ki):
@@ -109,11 +77,31 @@ def vec_ki(mag_ki, unit_vec_ki):
     return mag_ki * unit_vec_ki
 
 
-# -------- kf ---------
+
+
+calculate_ei = {
+    "source_to_monitor": source_to_monitor,
+    "vi": vi_from_one_monitor,
+    "ei": ei,
+    "monitor_to_sample": monitor_to_sample,
+    "time_on_sample": time_on_sample,
+    "unit_vec_ki": unit_vec_ki,
+    "mag_ki": mag_ki,
+    "vec_ki": vec_ki,
+}
+
+
+# -----------------------------------------------------
+# Calculate Ef
+# -----------------------------------------------------
+
 
 def sample_to_detectors(sample_position, detector_positions):
     d = detector_positions - sample_position
     return sc.norm(d)
+
+
+
 
 
 def unit_vec_kf(detector_positions, sample_position):
@@ -124,13 +112,6 @@ def unit_vec_kf(detector_positions, sample_position):
     return unit_kf
 
 
-def time_on_sample(sample_position, monitor_position, time_on_monitor, vi):
-    """ """
-    time_on_sample = time_on_monitor + sc.norm(sample_position - monitor_position) / vi
-    return time_on_sample
-
-
-
 def vec_vf(sample_to_detectors, tof, time_on_sample):
     """ """
     vf = sample_to_detectors / (tof - time_on_sample)
@@ -139,13 +120,28 @@ def vec_vf(sample_to_detectors, tof, time_on_sample):
 
 def mag_kf(vf):
     """ """
-    mag_kf = (sc.constants.neutron_mass * vf) / sc.constants.hbar
+    mag_kf = (consts.m_n * vf) / consts.hbar
     return sc.to_unit(mag_kf, "1/Å")
+
+
+def ef(vf):
+    ef = 0.5 * consts.m_n * vf**2
+    return sc.to_unit(ef, "meV")
 
 
 def vec_kf(mag_kf, unit_vec_kf):
     """ """
     return mag_kf * unit_vec_kf
+
+
+calculate_ef = {
+    "sample_to_detectors": sample_to_detectors,
+    "unit_vec_kf": unit_vec_kf,
+    "vf": vec_vf,
+    "ef": ef,
+    "mag_kf": mag_kf,
+    "vec_kf": vec_kf,
+}
 
 
 # --------- Q and en --------
@@ -156,32 +152,38 @@ def vec_Q(vec_ki, vec_kf):
     return vec_ki - vec_kf
 
 
+def qx(vec_Q):
+    return vec_Q.fields.x.copy()
+
+
+def qy(vec_Q):
+    return vec_Q.fields.y.copy()
+
+
+def qz(vec_Q):
+    return vec_Q.fields.z.copy()
+
+
 def mag_Q(vec_Q):
     """ """
     return sc.to_unit(sc.norm(vec_Q), "1/Å")
 
 
-def energy_transfer(mag_kf, mag_ki):
+def energy_transfer(ei, ef):
     """ """
-    en = (sc.constants.hbar**2 / (2 * sc.constants.neutron_mass)) * (
-        mag_ki**2 - mag_kf**2
-    )
+    en = ei - ef
     return sc.to_unit(en, "meV")
 
 
-dgs_reduction = {
-    "source_to_monitor": source_to_monitor,
-    "sample_to_detectors": sample_to_detectors,
-    "unit_vec_ki": unit_vec_ki,
-    "vi": vi_from_one_monitor,
-    "mag_ki": mag_ki,
-    "vec_ki": vec_ki,
-    "unit_vec_kf": unit_vec_kf,
-    "vf": vec_vf,
-    "mag_kf": mag_kf,
-    "vec_kf": vec_kf,
-    "vec_Q": vec_Q,
-    "mag_Q": mag_Q,
-    "en": energy_transfer,
-    "time_on_sample": time_on_sample,
-}
+dgs_reduction = (
+    calculate_ei
+    | calculate_ef
+    | {
+        "vec_Q": vec_Q,
+        "mag_Q": mag_Q,
+        "qx": qx,
+        "qy": qy,
+        "qz": qz,
+        "en": energy_transfer,
+    }
+)
